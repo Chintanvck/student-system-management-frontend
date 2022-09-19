84d5da974390088e4a2f788a01b95d8bae626eea
“20it058 <20it058@charusat.edu.in>
2022-09-19 13:12:31 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -4,8 +4,29 @@ import NavigationBar from './Components/NavigationBar'
 
 function App() {
   return (
-    <div>
-      <NavigationBar />
+    <div className="App">
+      <Router>
+        <NavigationBar />
+        <Container>
+          <Routes>
+            <Route
+              path="addpatient"
+              element={<AddPatient />} />
+            <Route
+              path="editpatient/:patientId"
+              element={<UpdatePatient />}
+            />
+            <Route
+              path="listpatient"
+              element={<PatientList />}
+            />
+            <Route
+              path="registerUser"
+              element={<RegistrationForm />}
+            />
+          </Routes>
+        </Container>
+      </Router>
     </div>
   );
 }
